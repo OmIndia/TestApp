d70bbce26e135e1288f4b4197c1c19b2deadf6f3
Om <chand_kw6@yahoo.com>
2017-05-01 20:19:57 +0530

    Seventh commit

--- signin_old (3).php
+++ signin_old (3).php
@@ -8,21 +8,8 @@ $pwd      = $_GET['pwd'];
 $pwd = md5($pwd);
 //echo $pwd;
 
-/*$link = mysqli_connect("localhost","root","hariom","test");
+$link = mysqli_connect("localhost","root","hariom","test");
        if (mysqli_connect_error()){
-        die( "Connection error");
-        }
-*/
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
- 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
- 
-//$conn = new mysqli($server, $username, $password, $db);
-$link = mysqli_connect($server, $username, $password, $db);
-if (mysqli_connect_error()){
         die( "Connection error");
         }
     //echo $uid;
