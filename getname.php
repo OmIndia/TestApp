46b16e05bd85bad7a0f681b2906ecfbbc206e549
Om <chand_kw6@yahoo.com>
2017-05-01 22:37:08 +0530

    11th commit

--- getname.php
+++ getname.php
@@ -23,50 +23,14 @@ if (mysqli_connect_error()){
         }
 
 
-    $query = "SELECT name from patients WHERE id=$id";
-    if ($result = mysqli_query($link,$query)) {
-
-        //echo "Query was successful";
-        $rowcount=mysqli_num_rows($result);
-        if ($rowcount > 0){
-            while ($row = mysqli_fetch_array($result)){
-                
-            //print_r($row);
-            // echo "<p> Patient Name is".$row['name']."</p>";   //works Apr 23 2017
-            //echo $row['name'];  works Apr 23 2017
-            //echo json_encode($row['name']);
-            echo $row['name']; 
-           }
     
-    } else  {
+$rtn = mysqli_fetch_assoc(mysqli_query($link,"SELECT name from patients WHERE `id`=$id"));
+ if ($rtn) {
+            $name = $rtn['name'];
+            echo $name;
+ }
+ else  {
         echo -1;  //does not work Apr 23 2017
     }
-    }
 
- /*       $rowcount=mysqli_num_rows($result);
-        if ($rowcount > 0){
-            echo "Success";
-        }else {
-            echo "Fail";
-        }
-    }
-    */
-    /*
-if(array_key_exists('callback', $_GET)){
-
-    header('Content-Type: text/javascript; charset=utf8');
-    header('Access-Control-Allow-Origin: http://www.example.com/');
-    header('Access-Control-Max-Age: 3628800');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-
-    $callback = $_GET['callback'];
-    echo $callback.'('.json_encode($result).');';
-
-}else{
-    // normal JSON string
-    header('Content-Type: application/json; charset=utf8');
-
-    echo json_encode($result);
-}
-*/
-?>
+ 
